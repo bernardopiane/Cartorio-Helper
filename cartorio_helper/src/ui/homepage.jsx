02982108988e6bf09312bb2fe7b20d9c879c525a
Bernardo Piane <bernardopiane@users.noreply.github.com>
2025-05-15 15:52:46 -0300

    Init

---
+++ cartorio_helper/src/ui/homepage.jsx
@@ -0,0 +1,19 @@
+import { DataGrid } from "@mui/x-data-grid";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <Paper sx={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{ pagination: { paginationModel } }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+          sx={{ border: 0 }}
+        />
+      </Paper>
+    </div>
+  );
+}
