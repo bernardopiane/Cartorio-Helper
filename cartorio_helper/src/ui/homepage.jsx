94c3f5e88b620e46038a1f19f6111028ba9b00db
Bernardo Piane <bernardopiane@users.noreply.github.com>
2025-05-19 21:08:35 -0300

    Implemented reading file; getting item code

--- cartorio_helper/src/ui/homepage.jsx
+++ cartorio_helper/src/ui/homepage.jsx
@@ -3,60 +3,143 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 function Homepage() {
+  const [data, setData] = React.useState([]);
+  const [file, setFile] = React.useState(null);
+
+  function handleFileChange(e) {
+    if (!e.target.files[0]) return;
+    setFile(e.target.files[0]);
+  }
+
+  function readFileContent(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        resolve(event.target.result);
+      };
+
+      reader.onerror = (error) => {
+        reject(error);
+      };
+
+      reader.readAsText(file);
+    });
+  }
+
+  async function loadData(file) {
+    var tempData = [];
+    if (!file) return;
+
+    try {
+      tempData = await readFileContent(file);
+    } catch (error) {
+      console.error("Error reading file:", error);
+      return;
+    } finally {
+      // Parse the XML file
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(tempData, 'text/xml');
+
+      // Get the data from the XML file
+      // Iterate through all the children from the Remessa node
+      const remessa = doc.getElementsByTagName('Remessa')[0];
+      const children = remessa.children;
+
+      console.log(children);
+      // Create a data array to hold the extracted information
+      const dataArray = [];
+
+      // Iterate through children elements
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+
+        // Extract common attributes that might be present in different certificate types
+        if (child.hasAttribute('Selo')) {
+          const recordData = getRecordData(child);
+
+          console.log(recordData);
+
+          dataArray.push(recordData);
+        }
+      }
+
+      setData(dataArray);
+
+    }
+  }
+
+  function getRecordData(child) {
+    const recordData = {
+      selo: child.getAttribute('Selo'),
+      codigo: getCodigo(child),
+      aleatorio: child.getAttribute('Aleatorio') || '',
+      livro: child.getAttribute('Livro'),
+      folha: child.getAttribute('Folha'),
+      termo: child.getAttribute('Termo'),
+      tipo: child.nodeName,
+    };
+
+    return recordData;
+  }
+
+  function getCodigo(child) {
+    switch (child.nodeName) {
+      case 'RegistroNascimento':
+        return "3001";
+      case 'RegistroObito':
+        return "3002";
+      case 'CertidaoNascimento':
+        return "3017";
+      case 'CertidaoCRCNacional':
+        return "3017CRC";
+      case 'CertidaoObito':
+        return "3017";
+      case 'HabilitacaoCasamento':
+        return "3003";
+      case 'CertidaoHabilitacaoCasamento':
+        return "3037";
+      case 'CertidaoCasamento':
+        return "3017";
+      case 'TranscricaoCasamento':
+        return "3007";
+      default:
+        return '';
+    }
+  }
+
   return (
     <div>
       <h1>Homepage</h1>
 
-      <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
-        <Column field="code" header="Code" sortable style={{ width: '25%' }}></Column>
-        <Column field="name" header="Name" sortable style={{ width: '25%' }}></Column>
-        <Column field="category" header="Category" sortable style={{ width: '25%' }}></Column>
-        <Column field="quantity" header="Quantity" sortable style={{ width: '25%' }}></Column>
+      {/* File picker */}
+      <input id="fileInput" type="file" onChange={handleFileChange} />
+      <button onClick={() => loadData(file)}>Carregar</button>
+
+      <DataTable value={data} tableStyle={{ minWidth: '50rem' }}>
+        <Column field="selo" header="Selo" sortable ></Column>
+        <Column field="codigo" header="Código" sortable ></Column>
+        <Column field="rcpn" header="RCPN" sortable ></Column>
+        <Column field="rit" header="RIT" sortable ></Column>
+        <Column field="protocolo" header="Protocolo" sortable ></Column>
+        <Column field="dataEntrada" header="Data de Entrada" sortable ></Column>
+        <Column field="pago" header="Pago" sortable ></Column>
+        <Column field="gratuito" header="Gratuito" sortable ></Column>
+        <Column field="livro" header="Livro" sortable ></Column>
+        <Column field="folha" header="Folha" sortable ></Column>
+        <Column field="termo" header="Termo" sortable ></Column>
+        <Column field="emolumentos" header="Emolumentos" sortable ></Column>
+        <Column field="lei3217" header="Lei 3217" sortable ></Column>
+        <Column field="lei4664" header="Lei 4664" sortable ></Column>
+        <Column field="lei111" header="Lei 111" sortable ></Column>
+        <Column field="funarpen" header="Funarpen" sortable ></Column>
+        <Column field="mutua" header="Mutua" sortable ></Column>
+        <Column field="acoterj" header="Acoterj" sortable ></Column>
+        <Column field="issqn" header="ISSQN" sortable ></Column>
+        <Column field="fiscalização" header="Fiscalização" sortable ></Column>
       </DataTable>
     </div>
   );
 }
-const products = [
-  { code: '101', name: 'Bamboo Watch', category: 'Accessories', quantity: 24 },
-  { code: '102', name: 'Black Watch', category: 'Accessories', quantity: 61 },
-  { code: '103', name: 'Blue Band', category: 'Accessories', quantity: 2 },
-  { code: '104', name: 'Blue T-Shirt', category: 'Clothing', quantity: 186 },
-  { code: '105', name: 'Bracket', category: 'Accessories', quantity: 15 },
-  { code: '106', name: 'Brown Purse', category: 'Accessories', quantity: 8 },
-  { code: '107', name: 'Chakra Band', category: 'Accessories', quantity: 34 },
-  { code: '108', name: 'Galaxy Band', category: 'Accessories', quantity: 84 },
-  { code: '109', name: 'Game Controller', category: 'Electronics', quantity: 267 },
-  { code: '110', name: 'Gloves', category: 'Clothing', quantity: 98 },
-  { code: '111', name: 'Green T-Shirt', category: 'Clothing', quantity: 10 },
-  { code: '112', name: 'Grey T-Shirt', category: 'Clothing', quantity: 50 },
-  { code: '113', name: 'Grey Watch', category: 'Accessories', quantity: 72 },
-  { code: '114', name: 'Headphone', category: 'Electronics', quantity: 8 },
-  { code: '115', name: 'Light Green T-Shirt', category: 'Clothing', quantity: 48 },
-  { code: '116', name: 'Lime Band', category: 'Accessories', quantity: 15 },
-  { code: '117', name: 'Lime T-Shirt', category: 'Clothing', quantity: 10 },
-  { code: '118', name: 'Mouse Pad', category: 'Accessories', quantity: 25 },
-  { code: '119', name: 'Navy Blue Watch', category: 'Accessories', quantity: 73 },
-  { code: '120', name: 'Orange Band', category: 'Accessories', quantity: 13 },
-  { code: '121', name: 'Orange Watch', category: 'Accessories', quantity: 81 },
-  { code: '122', name: 'Phone Case', category: 'Accessories', quantity: 88 },
-  { code: '123', name: 'Purple Band', category: 'Accessories', quantity: 32 },
-  { code: '124', name: 'Purple T-Shirt', category: 'Clothing', quantity: 2 },
-  { code: '125', name: 'Purple Watch', category: 'Accessories', quantity: 29 },
-  { code: '126', name: 'Red Band', category: 'Accessories', quantity: 15 },
-  { code: '127', name: 'Red Purse', category: 'Accessories', quantity: 42 },
-  { code: '128', name: 'Red T-Shirt', category: 'Clothing', quantity: 41 },
-  { code: '129', name: 'Red Watch', category: 'Accessories', quantity: 83 },
-  { code: '130', name: 'Running Shoe', category: 'Clothing', quantity: 20 },
-  { code: '131', name: 'Sneakers', category: 'Clothing', quantity: 60 },
-  { code: '132', name: 'Teal Band', category: 'Accessories', quantity: 30 },
-  { code: '133', name: 'Teal T-Shirt', category: 'Clothing', quantity: 25 },
-  { code: '134', name: 'Teal Watch', category: 'Accessories', quantity: 34 },
-  { code: '135', name: 'White Watch', category: 'Accessories', quantity: 33 },
-  { code: '136', name: 'Yellow Band', category: 'Accessories', quantity: 35 },
-  { code: '137', name: 'Yellow Purse', category: 'Accessories', quantity: 27 },
-  { code: '138', name: 'Yellow T-Shirt', category: 'Clothing', quantity: 46 },
-  { code: '139', name: 'Yellow Watch', category: 'Accessories', quantity: 89 },
-  { code: '140', name: 'Yo-Yo', category: 'Electronics', quantity: 11 },
-];
 
 export default Homepage;  
